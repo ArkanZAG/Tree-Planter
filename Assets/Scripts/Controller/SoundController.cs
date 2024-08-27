9189c18d5f764fdc0ab6c364ab865e1153dc2473
Arya Rakha <33478892+arrakh@users.noreply.github.com>
2024-08-27 13:31:16 +0700

    Implemented Sound Controller

--- Assets/Scripts/Controller/SoundController.cs
+++ Assets/Scripts/Controller/SoundController.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class SoundController : MonoBehaviour
+namespace Controller
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SoundController : MonoBehaviour
     {
+        private const string SFX_VOLUME_SETTING = "sfx-volume";
+        private const string BGM_VOLUME_SETTING = "bgm-volume";
         
-    }
+        [SerializeField] private AudioSource bgmSource, sfxSource;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private float sfxVolume = 1f;
+        private float bgmVolume = 1f;
+
+        private void Awake()
+        {
+            sfxVolume = PlayerPrefs.GetFloat(SFX_VOLUME_SETTING, 1f);
+            bgmVolume = PlayerPrefs.GetFloat(BGM_VOLUME_SETTING, 1f);
+        }
+
+        public void SetSfxVolume(float volume)
+        {
+            sfxVolume = volume;
+            PlayerPrefs.SetFloat(SFX_VOLUME_SETTING, volume);
+        }
+
+        public void SetBgmVolume(float volume)
+        {
+            bgmVolume = volume;
+            PlayerPrefs.SetFloat(BGM_VOLUME_SETTING, volume);
+        }
+
+        public void PlaySfx(AudioClip clip, float normalizedVolume = 1f)
+        {
+            sfxSource.volume = normalizedVolume * sfxVolume;
+            sfxSource.PlayOneShot(clip);
+        }
+
+        public void PlayBgm(AudioClip clip, float normalizedVolume = 1f)
+        {
+            bgmSource.Stop();
+            bgmSource.clip = clip;
+            bgmSource.volume = normalizedVolume * bgmVolume;
+            bgmSource.Play();
+        }
     }
 }
